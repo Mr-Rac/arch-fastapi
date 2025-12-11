4874c5cf4619cf5e5d7b5ad1b6aab9ce2df339b3
rac.yang <Mr.Rac@outlook.com>
2025-12-11 23:28:12 +0800

    perf(redis): optimize redis pool

--- app/api/dependencies/redis.py
+++ app/api/dependencies/redis.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import AsyncGenerator, Annotated
 
 from fastapi import Depends, HTTPException, Request, status
 from redis.asyncio import Redis
@@ -6,13 +6,18 @@ from redis.asyncio import Redis
 from app.domains.base_exception import Error
 
 
-async def get_auth_redis(request: Request) -> Redis:
+async def get_auth_redis(request: Request) -> AsyncGenerator[Redis, None]:
     if auth_redis_pool := getattr(request.state, "auth_redis_pool", None):
-        return Redis.from_pool(auth_redis_pool)
-    raise HTTPException(
-        status_code=status.HTTP_424_FAILED_DEPENDENCY,
-        detail=Error.INVALID_REDIS,
-    )
+        redis = Redis.from_pool(auth_redis_pool)
+        try:
+            yield redis
+        finally:
+            await redis.aclose()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_424_FAILED_DEPENDENCY,
+            detail=Error.INVALID_REDIS,
+        )
 
 
 AuthRedisDep = Annotated[Redis, Depends(get_auth_redis)]
